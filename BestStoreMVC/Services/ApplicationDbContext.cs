856749c7ffe533d928686135d8c573c9e8e8441e
praewa ainsuwan <praewa12800@gmail.com>
2024-11-07 12:31:20 +0700

    first commit

---
+++ BestStoreMVC/Services/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace BestStoreMVC.Services
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
